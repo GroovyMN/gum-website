b67a7503fa19c7dd240a3330904bd0e0b8fbf23f
Jeff Beck <jeff.beck@reachlocal.com>
2012-12-09 22:16:30 -0600

    Updated the site to use zip cache and min resources.

--- gum/grails-app/conf/BuildConfig.groovy
+++ gum/grails-app/conf/BuildConfig.groovy
@@ -7,50 +7,46 @@ grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // specify dependency exclusions here; for example, uncomment this to disable ehcache:
-        // excludes 'ehcache'
-    }
-    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    checksums true // Whether to verify checksums on resolve
+	// inherit Grails' default dependencies
+	inherits("global") {
+		// specify dependency exclusions here; for example, uncomment this to disable ehcache:
+		// excludes 'ehcache'
+	}
+	log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+	checksums true // Whether to verify checksums on resolve
 
-    repositories {
-        inherits true // Whether to inherit repository definitions from plugins
+	repositories {
+		inherits true // Whether to inherit repository definitions from plugins
 
-        grailsPlugins()
-        grailsHome()
-        grailsCentral()
+		grailsPlugins()
+		grailsHome()
+		grailsCentral()
 
-        mavenLocal()
-        mavenCentral()
+		mavenLocal()
+		mavenCentral()
 
-        // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
-    }
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+	}
+	dependencies {
+		// specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
-        // runtime 'mysql:mysql-connector-java:5.1.20'
-    }
+		// runtime 'mysql:mysql-connector-java:5.1.20'
+	}
 
-    plugins {
-        runtime ":hibernate:$grailsVersion"
-        runtime ":jquery:1.8.0"
-        runtime ":resources:1.1.6"
+	plugins {
+		runtime ":hibernate:$grailsVersion"
+		runtime ":jquery:1.8.0"
+		runtime ":resources:1.1.6"
 
-        // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0"
-        //runtime ":cached-resources:1.0"
-        //runtime ":yui-minify-resources:0.1.4"
 
-        build ":tomcat:$grailsVersion"
+		compile ":cache-headers:1.1.5"
+		runtime ":zipped-resources:1.0"
+		runtime ":cached-resources:1.0"
+		runtime ":yui-minify-resources:0.1.5"
 
-        runtime ":database-migration:1.1"
+		build ":tomcat:$grailsVersion"
 
-        compile ':cache:1.0.0'
-    }
+		runtime ":database-migration:1.1"
+
+		compile ':cache:1.0.0'
+	}
 }
