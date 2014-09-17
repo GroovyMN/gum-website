3ef2bd7396af3e4f7fc5aec69148c5d0077c32c8
staticnull <staticnull@gmail.com>
2014-09-17 05:45:17 -0500

    Rearrage sponsor attributes in domain

--- grails-app/domain/gum/Sponsor.groovy
+++ grails-app/domain/gum/Sponsor.groovy
@@ -1,18 +1,18 @@
 package gum
 
 class Sponsor {
+	String name
 	String type
 	String url
-	String name
 	String file
 	Date description
 	Byte photo
 	Integer sortOrder
 
 	static constraints = {
+		name(blank: false, nullable: false, unique: true)
 		type(blank: false, nullable: false)
 		url(url:true, blank: true, nullable: true)
-		name(blank: false, nullable: false, unique: true)
 		file(blank: false, nullable: false)
 		description(blank: true, nullable: true, maxSize: 1000)
 		photo(blank: true, nullable: true)
